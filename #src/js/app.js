f9cb5587c19639107dcc5b3a4a35549748f1f7ac
lexagreat <9608798010@mail.ru>
2024-10-18 11:37:34 +0300

    fix

--- #src/js/app.js
+++ #src/js/app.js
@@ -244,21 +244,124 @@ gsap.registerPlugin(ScrollTrigger);
 
 gsap.to(".hero__images .left", {
    scrollTrigger: {
-      trigger: ".hero__running", // элемент, который должен запускать анимацию
-      start: "top 70%", // когда верх элемента достигает 80% высоты экрана
+      trigger: ".hero__images", // элемент, который должен запускать анимацию
+      start: "top 50%", // когда верх элемента достигает 80% высоты экрана
       end: "bottom 0", // когда низ элемента достигает 20% высоты экрана
       // markers: true, // включить маркеры для визуальной отладки
       scrub: 1.5,
    },
-   x: -1000,
+   x: -400,
+   y: -500,
 });
 gsap.to(".hero__images .right", {
    scrollTrigger: {
-      trigger: ".hero__running", // элемент, который должен запускать анимацию
-      start: "top 70%", // когда верх элемента достигает 80% высоты экрана
+      trigger: ".hero__images", // элемент, который должен запускать анимацию
+      start: "top 50%", // когда верх элемента достигает 80% высоты экрана
       end: "bottom 0", // когда низ элемента достигает 20% высоты экрана
-      // markers: true, // включить маркеры для визуальной отладки
+      markers: true, // включить маркеры для визуальной отладки
       scrub: 1.5,
    },
-   x: 1000,
+   x: 400,
+   y: -500,
+});
+
+// Popup
+const popupLinks = document.querySelectorAll(".modal__link");
+const body = document.querySelector("body");
+const lockPadding = document.querySelectorAll(".lock-padding");
+const popupCloseIcon = document.querySelectorAll(".modal__close");
+
+let unlock = true;
+
+const timeout = 500;
+
+if (popupLinks.length > 0) {
+   for (let index = 0; index < popupLinks.length; index++) {
+      const popupLink = popupLinks[index];
+      popupLink.addEventListener("click", function (e) {
+         const popupName = popupLink.getAttribute("href").replace("#", "");
+         const curentPopup = document.getElementById(popupName);
+         popupOpen(curentPopup);
+         e.preventDefault();
+      });
+   }
+}
+
+if (popupCloseIcon.length > 0) {
+   for (let index = 0; index < popupCloseIcon.length; index++) {
+      const el = popupCloseIcon[index];
+      el.addEventListener("click", function (e) {
+         popupClose(el.closest(".modal"));
+         e.preventDefault();
+      });
+   }
+}
+
+function popupOpen(curentPopup) {
+   if (curentPopup && unlock) {
+      const popupActive = document.querySelector(".modal.open");
+      if (popupActive) {
+         popupClose(popupActive, false);
+      } else {
+         bodyLock();
+      }
+      curentPopup.classList.add("open");
+      curentPopup.addEventListener("click", function (e) {
+         if (!e.target.closest(".modal__content")) {
+            popupClose(e.target.closest(".modal"));
+         }
+      });
+   }
+}
+function popupClose(popupActive, doUnlock = true) {
+   if (unlock) {
+      popupActive.classList.remove("open");
+      if (doUnlock) {
+         bodyUnLock();
+      }
+   }
+}
+
+function bodyLock() {
+   const lockPaddingValue =
+      window.innerWidth - document.querySelector(".wrapper").offsetWidth + "px";
+
+   if (lockPadding.length > 0) {
+      for (let index = 0; index < lockPadding.length; index++) {
+         const el = lockPadding[index];
+         el.style.paddingRight = lockPaddingValue;
+      }
+   }
+   body.style.paddingRight = lockPaddingValue;
+   body.classList.add("lock");
+
+   unlock = false;
+   setTimeout(function () {
+      unlock = true;
+   }, timeout);
+}
+
+function bodyUnLock() {
+   setTimeout(function () {
+      if (lockPadding.length > 0) {
+         for (let index = 0; index < lockPadding.length; index++) {
+            const el = lockPadding[index];
+            el.style.paddingRight = "0px";
+         }
+      }
+      body.style.paddingRight = "0px";
+      body.classList.remove("lock");
+   }, timeout);
+
+   unlock = false;
+   setTimeout(function () {
+      unlock = true;
+   }, timeout);
+}
+
+document.addEventListener("keydown", function (e) {
+   if (e.which === 27) {
+      const popupActive = document.querySelector(".modal.open");
+      popupClose(popupActive);
+   }
 });
